72b1874f93581db4651a0c8a5896b511d4f9cad5
ijkgit <dlaworud@koreatech.ac.kr>
2023-02-10 21:36:23 +0900

    backend ver 1.0

---
+++ src/main/java/ParkLab/VMap/VMapApplication.java
@@ -0,0 +1,13 @@
+package ParkLab.VMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ParkLab/VMap/VMapApplicationTests.java
@@ -0,0 +1,13 @@
+package ParkLab.VMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
