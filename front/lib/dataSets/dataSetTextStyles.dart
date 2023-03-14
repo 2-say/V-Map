12685ef3cf04467b9910d38a935bea461f2f6c09
2-SangH <meer_kats@naver.com>
2023-03-14 15:38:34 +0900

    메인페이지 디자인 수정

--- front/lib/dataSets/dataSetTextStyles.dart
+++ front/lib/dataSets/dataSetTextStyles.dart
@@ -6,10 +6,19 @@ TextStyle h2 =
     const TextStyle(fontSize: 24, fontFamily: 'apeb', color: Colors.black);
 TextStyle h3 =
     const TextStyle(fontSize: 16, fontFamily: 'apeb', color: Colors.black);
-TextStyle b1 =
-    const TextStyle(fontSize: 20, fontFamily: 'apeb', color: Colors.black);
+TextStyle b1eb =
+    const TextStyle(fontSize: 16, fontFamily: 'apeb', color: Colors.black);
+TextStyle b1b =
+    const TextStyle(fontSize: 16, fontFamily: 'apb', color: Colors.black);
+TextStyle b1m =
+    const TextStyle(fontSize: 16, fontFamily: 'apm', color: Colors.black);
+TextStyle b1l =
+    const TextStyle(fontSize: 16, fontFamily: 'apl', color: Colors.black);
+TextStyle b1h =
+    const TextStyle(fontSize: 16, fontFamily: 'aph', color: Colors.black);
+
 TextStyle b1white =
-const TextStyle(fontSize: 20, fontFamily: 'apeb', color: Colors.white);
+    const TextStyle(fontSize: 20, fontFamily: 'apeb', color: Colors.white);
 TextStyle b2 =
     const TextStyle(fontSize: 16, fontFamily: 'apb', color: Colors.black);
 TextStyle b3 =
--- front/lib/pageFetures/pageFeaturesMain.dart
+++ front/lib/pageFetures/pageFeaturesMain.dart
@@ -13,9 +13,15 @@ class PageFeatureMain extends StatefulWidget {
 class _PageFeatureMainState extends State<PageFeatureMain> {
   //dataSet
   final List<String> statusCheckTitle = ['노션 연동', '스테레오 녹음', '마이크 녹음'];
+  final ScrollController agendaListViewScroller = ScrollController();
   late bool isRecordOn;
   late List<bool> statusCheck;
   late int time;
+  late String meetingName;
+  late String timeStamp;
+  late int participants;
+  late int currentTime;
+  late List<String> agendaList;
 
   @override
   void initState() {
@@ -25,6 +31,17 @@ class _PageFeatureMainState extends State<PageFeatureMain> {
     statusCheck = List.filled(3, false);
     //현재 녹음 시간 0으로 선언
     time = 0;
+    meetingName = '회의록 By V-Map';
+    timeStamp = '2023/03/14 · 16:00';
+    participants = 4;
+    currentTime = 0;
+    agendaList = [
+      '오늘 점심 메뉴 정하기',
+      '랩실 청소 당번 정하기',
+      '랩실 의자 교체 작업',
+      '연구 보고서 작성 회의일자 정하기',
+      '진행 과정 보고'
+    ];
     //이 함수가 실행 되어야 위젯 변수들의 초기화가 완료됨.
     super.initState();
   }
@@ -34,134 +51,185 @@ class _PageFeatureMainState extends State<PageFeatureMain> {
     return Scaffold(
       //전체를 감싸는 컨테이너, 배경색을 담당
       body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                colors: [ccKeyColorGreen, ccKeyColorCyan],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter)),
+        color: Colors.white,
         height: double.infinity,
         //자식들을 담을 column
-        child: Column(
-            //세로 정렬 : start( 시작점 부터 )
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //가로 정렬 : start ( 시작점 부터 )
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              //row:현재 회의 상태 표시
-              Row(
-                  children: List<Widget>.generate(statusCheck.length, (index) {
-                return Container(
-                    width: index == 1
-                        ? 180
-                        : index == 2
-                            ? 160
-                            : 150,
-                    child: ListTile(
-                      horizontalTitleGap: 0,
-                      onTap: () {
-                        print(statusCheck[index]);
-                      },
-                      title: Text(statusCheckTitle[index], style: b1white),
-                      trailing: Icon(
-                          statusCheck[index] == true
-                              ? Icons.check_box
-                              : Icons.close,
-                          color: statusCheck[index] == true
-                              ? Colors.green
-                              : Colors.redAccent),
-                    ));
-              })),
-              //container:회의 기록중 여부
-              Container(
-                  margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-                  child: Text("현재 V-MAP이 회의 내용을 노션에 기록 중 입니다.", style: h1)),
-              Container(
-                  height: 100,
-                  margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 2, color: Colors.black),
-                  ),
-                  child: Text(
-                    "회의제목: 회의록 by V-MAP",
-                    style: TextStyle(fontSize: 50),
-                  )),
-              Container(
-                  margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 2, color: Colors.black),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "주요안건: 회의내용을 바탕으로 자동요약된 안건입니다.",
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      Text(
-                        "AI-MAP 구현 가능성 평가",
-                      ),
-                      Icon(Icons.error),
-                      Text("AI-MAP에서 필요한 기능"),
-                      Icon(Icons.error),
-                      Text("AI-MAP의 정확도 향상 방법 평가"),
-                      Icon(Icons.error),
-                      Text("아이디어 경진대회 포스터 피드백"),
-                      Icon(Icons.error),
-                    ],
-                  )),
-              Container(
-                  margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 2, color: Colors.black),
-                  ),
-                  child: Text("회의내용: 회의 음성을 구분하여 텍스트화 한 내용입니다.")),
-              Container(
-                  width: double.infinity,
-                  margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 2, color: Colors.black),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Icon(
-                        Icons.square,
-                        color: Colors.red,
-                      ),
-                      Text("발언자1",
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+              //세로 정렬 : start( 시작점 부터 )
+              mainAxisAlignment: MainAxisAlignment.start,
+              //가로 정렬 : start ( 시작점 부터 )
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //row:현재 회의 상태 표시
+                Row(children: <Widget>[
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('회의명 : $meetingName', style: b1eb),
+                        Text(
+                          '$timeStamp · 참여자 $participants명',
+                          style: const TextStyle(
+                              fontFamily: 'apb', color: Colors.grey),
+                        )
+                      ]),
+                  const Expanded(child: SizedBox()),
+                  Text('$currentTime', style: b1eb),
+                  const SizedBox(width: 12),
+                  isRecordOn
+                      ? const Text('녹음 중',
                           style: TextStyle(
-                            fontSize: 20,
+                              fontFamily: 'apeb', color: Colors.blueAccent))
+                      : const Text('일시 정지 중',
+                          style: TextStyle(
+                              fontFamily: 'apeb', color: Colors.redAccent)),
+                  const SizedBox(width: 8),
+                  Row(
+                    children: List<Widget>.generate(participants, (index) {
+                      return const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 4.0),
+                        child: CircleAvatar(
+                            backgroundColor: Colors.blue, radius: 16),
+                      );
+                    }),
+                  )
+                ]),
+                const SizedBox(height: 16),
+                Container(
+                  width: double.infinity,
+                  height: 80,
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Image.asset('assets/wave.png'),
+                ),
+                const SizedBox(height: 16),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  height: 48,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                      border: Border.symmetric(
+                          horizontal:
+                              BorderSide(width: 1, color: Colors.grey))),
+                  child: Text('자동 요약 안건', style: b1eb),
+                ),
+                Container(
+                    padding: const EdgeInsets.all(4.0),
+                    height: 136,
+                    child: Scrollbar(
+                      controller: agendaListViewScroller,
+                      thumbVisibility: true,
+                      child: SingleChildScrollView(
+                          controller: agendaListViewScroller,
+                          child: Column(
+                            children: List<Widget>.generate(5, (index) {
+                              return ListTile(
+                                  dense: true,
+                                  visualDensity:
+                                      const VisualDensity(vertical: -3),
+                                  title: Text(
+                                    agendaList[index],
+                                    style: const TextStyle(
+                                        fontFamily: 'apm', color: Colors.grey),
+                                  ),
+                                  trailing: IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.close)));
+                            }),
                           )),
-                      Text(
-                          "지금부터 회의 시작하겠습니다. 오늘 회의주제는 공학 설계 아이디어 조사 내용 발표이며, 발표 이후에는 교수님의 피드백이 있을 예정입니다."),
-                      Icon(
-                        Icons.square,
-                        color: Colors.blue,
-                      ),
-                      Text("발언자2", style: TextStyle(fontSize: 20)),
-                      Text("발표 시작하겠습니다.\n저희팀의 주제는 음성인식 기반 회의록 자동 생성 프로그램입니다."),
-                      Icon(
-                        Icons.square,
-                        color: Colors.blue,
-                      ),
-                      Text("발언자2", style: TextStyle(fontSize: 20)),
-                      Text(
-                          "우선 아이디어 조사 내용을발표하기에 앞서 저희 팀의 주제에 대한 간단한 소개를 하겠습니다."),
-                      Container(
-                        width: 1000,
-                        height: 60,
-                        color: Colors.red,
-                        margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
-                      ),
-                      Container(
-                        width: 1000,
-                        height: 80,
-                        color: Colors.red,
-                        margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
-                      )
-                    ],
-                  )),
-            ]),
+                    )),
+                const SizedBox(height: 16),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  height: 48,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                      border: Border.symmetric(
+                          horizontal:
+                              BorderSide(width: 1, color: Colors.grey))),
+                  child: Text('회의 내용', style: b1eb),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  height: 32,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(width: 1, color: Colors.grey))),
+                  child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        const Text('편집 도구',
+                            style: TextStyle(
+                                fontFamily: 'apeb', color: Colors.grey)),
+                        Expanded(
+                          child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.title),
+                                constraints: const BoxConstraints(),
+                                padding: EdgeInsets.zero,
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.view_agenda),
+                                constraints: const BoxConstraints(),
+                                padding: EdgeInsets.zero,
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.edit,color: Colors.blueAccent),
+                                constraints: const BoxConstraints(),
+                                padding: EdgeInsets.zero,
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.error,color: Colors.redAccent),
+                                constraints: const BoxConstraints(),
+                                padding: EdgeInsets.zero,
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.notifications_on),
+                                constraints: const BoxConstraints(),
+                                padding: EdgeInsets.zero,
+                              ),
+                            ],
+                          ),
+                        )
+                      ]),
+                ),
+                Expanded(
+                    child: Container(
+                  child: SingleChildScrollView(),
+                )),
+                Container(
+                  width: double.infinity,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                    isRecordOn ? ccKeyColorGreen : Colors.red,
+                    ccKeyColorCyan
+                  ], begin: Alignment.centerLeft, end: Alignment.centerRight)),
+                  child: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          isRecordOn = !isRecordOn;
+                        });
+                      },
+                      icon: Icon(
+                          isRecordOn
+                              ? Icons.pause_circle_outline
+                              : Icons.play_circle_outline,
+                          size: 36,
+                          color: Colors.white)),
+                )
+              ]),
+        ),
       ),
     );
   }
