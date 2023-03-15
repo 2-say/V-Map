f795bca99476eabc84252474f8a8d68b89714bae
Jay <dlaworud@koreatech.ac.kr>
2023-03-15 20:46:54 +0900

    Update Main.java

--- src/main/java/ParkLab/VMap/controller/notion/Main.java
+++ src/main/java/ParkLab/VMap/controller/notion/Main.java
@@ -9,7 +9,7 @@ public class Main {
         List<String> contents = new ArrayList<>();
         contents.add("apple");  //0
         contents.add("banana"); //1
-        contents.add("orange"); //2
+        contents.add("orange"); //깃 데스크톱 테스트
         notionApi.createBlock(contents);
     }
 }
