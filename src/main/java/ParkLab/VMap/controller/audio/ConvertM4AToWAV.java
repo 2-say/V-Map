db8b8284c79fccbcc8a9f97dfdff94141c179d63
sehee <2-say>
2023-03-20 11:43:19 +0900

    ConvertM4AToWav 작동 잘됌.

--- src/main/java/ParkLab/VMap/controller/audio/ConvertM4AToWAV.java
+++ src/main/java/ParkLab/VMap/controller/audio/ConvertM4AToWAV.java
@@ -8,13 +8,20 @@ public class ConvertM4AToWAV {
     public static void main(String[] args) {
         try {
             // m4a 파일 경로
-            String sourceFilePath = "/Users/isehui/Documents/Zoom/2023-03-19 13.58.23 세희 이님의 Zoom 회의/Audio Record/sample1.m4a";
+            String sourceFilePath = "/Users/isehui/develop/V-map_backend_clone/sample1.m4a";
 
             // 변환될 wav 파일 경로
-            String targetFilePath = "/Users/isehui/Documents/Zoom/2023-03-19 13.58.23 세희 이님의 Zoom 회의/Audio Record/sample2.m4a";
+            String targetFilePath = "/Users/isehui/develop/V-map_backend_clone/test111.wav";
+            Process process = Runtime.getRuntime().exec("/bin/bash -c 'eval $(/opt/homebrew/bin/brew shellenv)'");
+            String line1;
+            BufferedReader input1 = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            while ((line1 = input1.readLine()) != null) {
+                System.out.println(line1);
+            }
+            input1.close();
 
             // FFmpeg 실행
-            String[] cmd = {"ffmpeg", "-i", sourceFilePath, "-vn", "-acodec", "pcm_s16le", "-ar", "44100", "-ac", "2", targetFilePath};
+            String[] cmd = {"/opt/homebrew/Cellar/ffmpeg/5.1.2_6/bin/ffmpeg", "-i", sourceFilePath, "-vn", "-acodec", "pcm_s16le", "-ar", "44100", "-ac", "2", targetFilePath};
             Process p = Runtime.getRuntime().exec(cmd);
 
             // 실행 결과 출력
