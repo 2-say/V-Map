9d17eaa5f7a7c69ec4d8439a9f766ad13f8dee6e
Jay <dlaworud@koreatech.ac.kr>
2023-03-20 14:20:50 +0900

    경로 수정
    절대경로 -> 상대경로로 통일

---
+++ src/main/java/ParkLab/VMap/controller/audio/ConvertM4AToWAV.java
@@ -0,0 +1,40 @@
+package ParkLab.VMap.controller.audio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConvertM4AToWAV {
+    public static void main(String[] args) {
+        try {
+            // m4a 파일 경로
+            String sourceFilePath = "./audio/sample1.m4a";
+
+            // 변환될 wav 파일 경로
+            String targetFilePath = "./audio/test111.wav";
+            Process process = Runtime.getRuntime().exec("/bin/bash -c 'eval $(/opt/homebrew/bin/brew shellenv)'");
+            String line1;
+            BufferedReader input1 = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            while ((line1 = input1.readLine()) != null) {
+                System.out.println(line1);
+            }
+            input1.close();
+
+            // FFmpeg 실행
+            String[] cmd = {"/opt/homebrew/Cellar/ffmpeg/5.1.2_6/bin/ffmpeg", "-i", sourceFilePath, "-vn", "-acodec", "pcm_s16le", "-ar", "44100", "-ac", "2", targetFilePath};
+            Process p = Runtime.getRuntime().exec(cmd);
+
+            // 실행 결과 출력
+            String line;
+            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            while ((line = input.readLine()) != null) {
+                System.out.println(line);
+            }
+            input.close();
+
+            System.out.println("M4A to WAV conversion succeeded!");
+        } catch (IOException e) {
+            System.err.println("M4A to WAV conversion failed: " + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/ParkLab/VMap/controller/audio/MultiChannelAudioMerger.java
@@ -0,0 +1,78 @@
+package ParkLab.VMap.controller.audio;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//채널 합치는 곳
+public class MultiChannelAudioMerger {
+    public static void main(String[] args) {
+        try {
+            // Input files
+            File inputFile1 = new File("./audio/sample1.wav");
+            File inputFile2 = new File("./audio/sample2.wav");
+            ArrayList<File> inputFiles = new ArrayList<>(Arrays.asList(inputFile1, inputFile2));
+
+            // Output file
+            File outputFile = new File("./audio/output.wav");
+
+            // Audio format
+            AudioFormat audioFormat = AudioSystem.getAudioInputStream(inputFile1).getFormat();
+
+            // Create an audio input stream for each file
+            ArrayList<AudioInputStream> audioInputStreams = new ArrayList<>();
+            for (File inputFile : inputFiles) {
+                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(inputFile);
+                audioInputStreams.add(audioInputStream);
+            }
+
+            // Merge audio input streams
+            AudioInputStream audioInputStream = new AudioInputStream(
+                    new MultiAudioInputStream(audioInputStreams),
+                    audioFormat,
+                    AudioSystem.getAudioInputStream(inputFile1).getFrameLength() + AudioSystem.getAudioInputStream(inputFile2).getFrameLength()
+            );
+
+            // Write output file
+            AudioSystem.write(audioInputStream, AudioFileFormat.Type.WAVE, outputFile);
+
+            // Close audio input streams
+            for (AudioInputStream audioInputStream2 : audioInputStreams) {
+                audioInputStream2.close();
+                System.out.println("채널파일 제작 완료");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class MultiAudioInputStream extends AudioInputStream {
+        private final ArrayList<AudioInputStream> audioInputStreams;
+        private int currentIndex = 0;
+
+        public MultiAudioInputStream(ArrayList<AudioInputStream> audioInputStreams) {
+            super(audioInputStreams.get(0), audioInputStreams.get(0).getFormat(), Long.MAX_VALUE);
+            this.audioInputStreams = audioInputStreams;
+        }
+
+        public int read(byte[] b, int off, int len) throws IOException {
+            int bytesRead = audioInputStreams.get(currentIndex).read(b, off, len);
+            if (bytesRead == -1 && currentIndex + 1 < audioInputStreams.size()) {
+                currentIndex++;
+                bytesRead = read(b, off, len);
+            }
+            return bytesRead;
+        }
+
+        public void close() throws IOException {
+            for (AudioInputStream audioInputStream : audioInputStreams) {
+                audioInputStream.close();
+            }
+        }
+    }
+}
