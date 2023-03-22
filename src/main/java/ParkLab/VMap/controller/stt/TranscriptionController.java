dc5c517cfc7f43b650b98a6882f6bb45c6b6c278
Jay <dlaworud@koreatech.ac.kr>
2023-03-22 14:37:31 +0900

    MVC 분리 + 테스트 page + txt 파일로 스토리지 저장

--- src/main/java/ParkLab/VMap/controller/stt/TranscriptionController.java
+++ src/main/java/ParkLab/VMap/controller/stt/TranscriptionController.java
@@ -4,25 +4,34 @@ import ParkLab.VMap.model.stt.AuthSample;
 import ParkLab.VMap.model.stt.GetTranscribeSample;
 import ParkLab.VMap.model.stt.PostTranscribeSample;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Controller
-@Configuration
 public class TranscriptionController {
 
-    @GetMapping("/")
+    @GetMapping("/index.html")
     public String index() {
         return "index";
     }
 
-    @GetMapping("/transcribe")
+    @GetMapping("/transcribe.html")
     public String transcribe(Model model) throws Exception {
         AuthSample authSample = new AuthSample();
         String auth = authSample.getAuth();
@@ -45,12 +54,36 @@ public class TranscriptionController {
             transcription += matcher.group(1);
         }
 
+        String userId = "Jay";
+        saveText(userId, transcription);
+
         ObjectMapper objectMapper = new ObjectMapper();
         String transcriptionJson = objectMapper.writeValueAsString(transcription);
 
-        model.addAttribute("transcribe", transcriptionJson);
+        model.addAttribute("transcription", transcriptionJson);
 
         return "transcribe";
     }
 
+    @PostMapping("/saveText")
+    public ResponseEntity<String> saveText(@RequestParam("userId") String userId,
+                                           @RequestBody String text) throws IOException {
+
+        // 회원 정보를 통해 파일 이름 생성
+        String fileName = "transcription_" + userId + "_" + new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Date()) + ".txt";
+        String filePath = "./transcriptions/" + fileName;
+
+        try {
+            // 생성된 파일에 텍스트 저장
+            FileWriter fileWriter = new FileWriter(filePath);
+            fileWriter.write(text);
+            fileWriter.close();
+
+            return new ResponseEntity<>("Text saved successfully for user " + userId, HttpStatus.OK);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Error saving text for user " + userId, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
