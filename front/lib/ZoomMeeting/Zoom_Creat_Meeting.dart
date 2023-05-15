56cf78145be288a777b1c5844386493d577c0507
wonhee44 <bbq0322@naver.com>
2023-05-15 22:12:07 +0900

    원희 작업물

--- front/lib/ZoomMeeting/Zoom_Creat_Meeting.dart
+++ front/lib/ZoomMeeting/Zoom_Creat_Meeting.dart
@@ -1,82 +1,106 @@
 import 'dart:async';
 import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:crypto/crypto.dart';
 
-void main() {
-  createZoomMeeting();
-}
 
-void createZoomMeeting() async {
-  final apiKey = 'ru1ye_lgTMWHIiPiY6G7SQ';
-  final apiSecret = 'c0hcgeYFsHjNNJ0DOG2EvbKOtQGOqxVrDHXN';
-  final zoomUserId = 'bbq0322@naver.com';
 
-  // JWT 토큰 생성
-  final payload = {
-    'iss': apiKey,
-    'exp': DateTime.now().toUtc().add(Duration(hours: 2)).millisecondsSinceEpoch ~/ 1000,
-  };
+class ZoomMeetingCreator{
 
-  final base64UrlHeader = base64Url.encode(utf8.encode(json.encode({'alg': 'HS256', 'typ': 'JWT'})));
-  final base64UrlPayload = base64Url.encode(utf8.encode(json.encode(payload)));
-  final signingInput = '$base64UrlHeader.$base64UrlPayload';
-  final hmacSha256 = Hmac(sha256, utf8.encode(apiSecret));
-  final base64UrlSignature = base64Url.encode(hmacSha256.convert(utf8.encode(signingInput)).bytes);
-  final token = '$signingInput.$base64UrlSignature';
+  String? startUrl;
+  String? joinUrl;
 
-  // Zoom API를 이용한 회의 생성
-  final url = 'https://api.zoom.us/v2/users/$zoomUserId/meetings';
 
-  final headers = {
-    'Authorization': 'Bearer $token',
-    'Content-Type': 'application/json',
-  };
+  Future<void> createZoomMeeting() async {
+    final apiKey = 'ru1ye_lgTMWHIiPiY6G7SQ';
+    final apiSecret = 'c0hcgeYFsHjNNJ0DOG2EvbKOtQGOqxVrDHXN';
+    final zoomUserId = 'bbq0322@naver.com';
 
-  final data = {
-    'topic': 'Zoom Meeting',
-    'type': 2,
-    'start_time': '2023-05-03T13:00:00Z',
-    'duration': 60,
-    'timezone': 'Asia/Seoul',
-    'agenda': 'This is a test meeting'
-  };
+    // JWT 토큰 생성
+    final payload = {
+      'iss': apiKey,
+      'exp': DateTime.now().toUtc().add(Duration(hours: 2)).millisecondsSinceEpoch ~/ 1000,
+    };
 
-  final response = await http.post(Uri.parse(url), headers: headers, body: json.encode(data));
+    final base64UrlHeader = base64Url.encode(utf8.encode(json.encode({'alg': 'HS256', 'typ': 'JWT'})));
+    final base64UrlPayload = base64Url.encode(utf8.encode(json.encode(payload)));
+    final signingInput = '$base64UrlHeader.$base64UrlPayload';
+    final hmacSha256 = Hmac(sha256, utf8.encode(apiSecret));
+    final base64UrlSignature = base64Url.encode(hmacSha256.convert(utf8.encode(signingInput)).bytes);
+    final token = '$signingInput.$base64UrlSignature';
 
-  if (response.statusCode == 201) {
-    final responseData = json.decode(response.body);
-    final startUrl = responseData['start_url'];
-    final joinUrl = responseData['join_url'];
-    final startTimeString = responseData['start_time'];
-    final topic = responseData['topic'];
-    final createdAt = responseData['created_at'];
-    final password = responseData['password'];
-    final startTime = DateTime.parse(startTimeString);
+    // Zoom API를 이용한 회의 생성
+    final url = 'https://api.zoom.us/v2/users/$zoomUserId/meetings';
 
-    final duration = int.parse(data['duration'].toString());
-    final endTime = startTime.add(Duration(minutes: duration));
+    final headers = {
+      'Authorization': 'Bearer $token',
+      'Content-Type': 'application/json',
+    };
 
-    print('Start URL: $startUrl');
-    print('Join URL: $joinUrl');
-    print('Start Time: $startTime');
-    print('End Time: $endTime');
-    print('Topic: $topic');
-    print('Created At: $createdAt');
-    print('Password: $password');
+    final data = {
+      'topic': 'Zoom Meeting',
+      'type': 2,
+      'start_time': '2023-05-03T13:00:00Z',
+      'duration': 60,
+      'timezone': 'Asia/Seoul',
+      'agenda': 'This is a test meeting'
+    };
 
-    // 회의 종료까지 기다림
-    await Future.delayed(Duration(minutes: duration));
+    final response = await http.post(Uri.parse(url), headers: headers, body: json.encode(data));
 
-    final endUrl = 'https://api.zoom.us/v2/meetings/${responseData['id']}/status';
-    final endResponse = await http.put(Uri.parse(endUrl), headers: headers, body: json.encode({'action': 'end'}));
+    if (response.statusCode == 201) {
+      final responseData = json.decode(response.body);
+      startUrl = responseData['start_url'];
+      joinUrl = responseData['join_url'];
+      final startTimeString = responseData['start_time'];
+      final topic = responseData['topic'];
+      final createdAt = responseData['created_at'];
+      final password = responseData['password'];
+      final startTime = DateTime.parse(startTimeString);
 
-    if (endResponse.statusCode == 204) {
-      print('Meeting has ended at ${DateTime.now()}');
+      final duration = int.parse(data['duration'].toString());
+      final endTime = startTime.add(Duration(minutes: duration));
+
+      print('Start URL: $startUrl');
+      print('Join URL: $joinUrl');
+      print('Start Time: $startTime');
+      print('End Time: $endTime');
+      print('Topic: $topic');
+      print('Created At: $createdAt');
+      print('Password: $password');
+
+      print("여기보세여");
+      print(startUrl);
+      //return [startUrl, joinUrl];
+
+      // 회의 종료까지 기다림
+     // await Future.delayed(Duration(minutes: duration));
+
+     // final endUrl = 'https://api.zoom.us/v2/meetings/${responseData['id']}/status';
+    //  final endResponse = await http.put(Uri.parse(endUrl), headers: headers, body: json.encode({'action': 'end'}));
+
+    //  if (endResponse.statusCode == 204) {
+    //    print('Meeting has ended at ${DateTime.now()}');
+
+        // Return startUrl and joinUrl
+        
+
+    //  } else {
+    //    print('Failed to end Zoom meeting. Status code: ${endResponse.statusCode}');
+  //    }
     } else {
-      print('Failed to end Zoom meeting. Status code: ${endResponse.statusCode}');
+      print('Failed to create Zoom meeting. Status code: ${response.statusCode}');
     }
 
-  } else {
-    print('Failed to create Zoom meeting. Status code: ${response.statusCode}');
-  }}
+    // Return empty list if meeting creation or end fails
+  }
+
+
+
+}
+void main(){
+
+  ZoomMeetingCreator a = ZoomMeetingCreator();
+  a.createZoomMeeting();
+}
