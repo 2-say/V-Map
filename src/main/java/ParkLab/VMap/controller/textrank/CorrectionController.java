455b9088c32930a468edc3ce1def0ea56c4835e0
sehee <2-say>
2023-05-26 17:36:44 +0900

    CorrectionController 수정

--- src/main/java/ParkLab/VMap/controller/textrank/CorrectionController.java
+++ src/main/java/ParkLab/VMap/controller/textrank/CorrectionController.java
@@ -1,18 +1,21 @@
 package ParkLab.VMap.controller.textrank;
 
-import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+@RestController
 public class CorrectionController {
 
     @GetMapping("/Correction")
-    public static void correction(@RequestParam String word) {
+    @ResponseBody
+    public String correction(@RequestParam String word) {
         String apiUrl = "http://127.0.0.1:5000/correction?word=" + word;
 
         HttpClient client = HttpClient.newHttpClient();
@@ -24,10 +27,13 @@ public class CorrectionController {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             System.out.println("Status code: " + response.statusCode());
             System.out.println("Response body: " + response.body());
-            JSONObject json = new JSONObject(); //JSON 교정 return 필요
+
+            return response.body();
+
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return null;
     }
 }
